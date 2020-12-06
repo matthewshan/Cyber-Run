ef9381c3e47e2681c49c4d12e71bb532ac28bc1e
KyleVanDenHeuvel <vandenhk@mail.gvsu.edu>
2020-12-05 22:26:55 -0500

    Score board and health

--- Cyber;Run/Assets/ObstacleSpawner.cs
+++ Cyber;Run/Assets/ObstacleSpawner.cs
@@ -4,29 +4,29 @@ using UnityEngine;
 
 public class ObstacleSpawner : MonoBehaviour
 {
-    public float interval;
     public GameObject obstacle;
-    public float height;
+    public float spawnInterval;    
+    public float heightVariance;
 
     private float timer;
 
     // Start is called before the first frame update
     void Start()
     {
-        timer = interval;
+        timer = spawnInterval; // Initialize  the timer
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (timer > interval)
+        if (timer > spawnInterval) // Check if the timer has reached the specified spawnInterval
         {
-            GameObject newObstacle = Instantiate(obstacle);
-            newObstacle.transform.position = transform.position + new Vector3(0, Random.Range(-height, height), 0);
-            Destroy(newObstacle, 10);
-            timer = 0;
+            GameObject newObstacle = Instantiate(obstacle); //Creates a new obstacle
+            newObstacle.transform.position = transform.position + new Vector3(0, Random.Range(-heightVariance, heightVariance), 0); //Sets the position a random height and the transformat position
+            Destroy(newObstacle, 10); //Destroy the new obstacle after 10 seconds
+            timer = 0; //Reset the timer to zero
         }
 
-        timer += Time.deltaTime;
+        timer += Time.deltaTime; //Add to the timer the amount of passed time
     }
 }
